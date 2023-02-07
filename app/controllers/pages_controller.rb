d18ebd48ff2643223967df0cf0a3bd0789f6c8c9
Somesh Yadav <soyadav@bestpeers.com>
2023-02-07 18:42:52 +0530

    Create connection with faraday and added routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def create_post
+    # faraday post to localhost:3001
+    conn = Faraday.new(url: 'http://localhost:3001')
+
+    response = conn.post do |req|
+      req.url '/posts'
+      req.headers['Content-Type'] = 'application/json'
+      req.body = {
+        post: 
+        {
+          title: params[:title],
+          views: 0
+        }}.to_json
+    end
+    JSON.parse(response.body)
+    redirect_to root_path, notice: 'Post created'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
+  post 'create_post', to: 'pages#create_post'
+  delete 'destroy_post/:id', to: 'pages#destroy_post', as: 'destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
